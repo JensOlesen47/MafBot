a77f9da6a7d8e614edf2fb337612c4f65f11fb9d
Jens Olesen <JensOlesen47@mgail.com>
2019-12-03 00:17:29 -0500

    WIP

--- http/http.ts
+++ http/http.ts
@@ -17,14 +17,16 @@ http.createServer((req, res) => {
     console.log(req.url);
     console.log(code);
     data.code = code;
+    console.log(data);
     api.post('https://discordapp.com/api/oauth2/token', data, { headers }).then(fulfilled => {
         console.log(fulfilled.data);
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(html);
         res.end();
     }).catch(rejected => {
-        console.log(rejected);
-        res.write(JSON.stringify(rejected.data));
+        res.write('oops');
         res.end();
     });
 }).listen(8080);
+
+console.log('reddy');
