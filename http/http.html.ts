5d2b2ab12a5390629a9355f7848747365f78c877
Jens Olesen <JensOlesen47@mgail.com>
2019-12-02 21:56:23 -0500

    Add basic http server

---
+++ http/http.html.ts
@@ -0,0 +1,12 @@
+export const html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Registration confirmed</title>
+</head>
+<body>
+<p>Congrats, you are now registered with mafbot!</p>
+</body>
+</html>
+`;
---
+++ http/http.ts
@@ -0,0 +1,8 @@
+const http = require('http');
+import {html} from './http.html';
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(html);
+    res.end();
+}).listen(8080);
