b4c71cfb0778dcf65d3c5800eb80538fc5225710
Jens Olesen <JensOlesen47@mgail.com>
2019-12-03 00:37:44 -0500

    WIP

--- http/http.ts
+++ http/http.ts
@@ -3,20 +3,12 @@ const api = require('axios').default;
 import {html} from './http.html';
 
 const headers = {'Content-Type':'application/x-www-form-urlencoded'};
-const data = {
-    'client_id': '487077607427276810',
-    'client_secret': 'Ai4r9jbq2cG24U5Oibzd1t4T5wHzDENm',
-    'grant_type': 'authorization_code',
-    'redirect_uri': encodeURIComponent('http://18.223.209.141:8080/'),
-    'scope': 'identify gdm.join',
-    'code': null
-};
 
 http.createServer((req, res) => {
     const code = req.url.split('?code=')[1];
     console.log(req.url);
     console.log(code);
-    data.code = code;
+    const data = `client_id=487077607427276810&client_secret=Ai4r9jbq2cG24U5Oibzd1t4T5wHzDENm&grant_type=authorization_code&redirect_uri=${encodeURIComponent('http://18.223.209.141:8080/')}&scope=${encodeURIComponent('identify gdm.join')}&code=${code}`;
     console.log(data);
     api.post('https://discordapp.com/api/oauth2/token', data, { headers }).then(fulfilled => {
         console.log(fulfilled.data);
