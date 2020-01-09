0d3d3351f39cb298010e8f871819d40b589b45a4
Jens Olesen <JensOlesen47@mgail.com>
2020-01-09 15:03:58 -0500

    Minor fix to test vote page

--- http/http.html.ts
+++ http/http.html.ts
@@ -1,6 +1,6 @@
 const htmlPages = {
     registrationConfirmed: `<p>Congrats, you are now registered with mafbot!</p>`,
-    vote: `<label for="votee" style="margin-right: 25px">Who do you want to vote for?</label><select id="votee" onchange="document.getElementById('voteeSpan').innerHTML = 'document.getElementById('votee').value"><option value="Urist">Urist</option><option value="StarV">StarV</option><option value="Ellibereth">Ellibereth</option></select><br/><br/>You are now voting for: <span id="voteeSpan"></span>`
+    vote: `<label for="votee" style="margin-right: 25px">Who do you want to vote for?</label><select id="votee" onchange="document.getElementById('voteeSpan').innerHTML = document.getElementById('votee').value"><option value="Urist">Urist</option><option value="StarV">StarV</option><option value="Ellibereth">Ellibereth</option></select><br/><br/>You are now voting for: <span id="voteeSpan"></span>`
 };
 
 export function getHtmlPage (urlSegment: string) : string {
