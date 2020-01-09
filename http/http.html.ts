a8a93fddf17870ef0ec77675070714bbfa64031b
Jens Olesen <JensOlesen47@mgail.com>
2020-01-09 14:57:03 -0500

    Test vote page

--- http/http.html.ts
+++ http/http.html.ts
@@ -1,12 +1,23 @@
-export const html = `
+const htmlPages = {
+    registrationConfirmed: `<p>Congrats, you are now registered with mafbot!</p>`,
+    vote: `<label for="votee" style="margin-right: 25px">Who do you want to vote for?</label><select id="votee" onchange="document.getElementById('voteeSpan').innerHTML = document.getElementById('votee').innerHTML"><option value="Urist">Urist</option><option value="StarV">StarV</option><option value="Ellibereth">Ellibereth</option></select><br/><br/><span id="voteeSpan"></span>`
+};
+
+export function getHtmlPage (urlSegment: string) : string {
+    const pageBody = htmlPages[urlSegment];
+    if (!pageBody) {
+        return;
+    }
+
+    return `
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Registration confirmed</title>
-</head>
-<body>
-<p>Congrats, you are now registered with mafbot!</p>
-</body>
-</html>
-`;
+    <head>
+        <meta charset="UTF-8">
+        <title>${urlSegment}</title>
+    </head>
+    <body>
+        ${pageBody}
+    </body>
+</html>`;
+}
--- http/http.ts
+++ http/http.ts
@@ -1,24 +1,35 @@
 const http = require('http');
 const api = require('axios').default;
-import {html} from './http.html';
+import {getHtmlPage} from './http.html';
 
 const headers = {'Content-Type':'application/x-www-form-urlencoded'};
 
 http.createServer((req, res) => {
-    const code = req.url.split('?code=')[1];
-    console.log(req.url);
-    console.log(code);
-    const data = `client_id=487077607427276810&client_secret=Ai4r9jbq2cG24U5Oibzd1t4T5wHzDENm&grant_type=authorization_code&redirect_uri=${encodeURIComponent('http://18.223.209.141:8080/')}&scope=${encodeURIComponent('identify gdm.join')}&code=${code}`;
-    console.log(data);
-    api.post('https://discordapp.com/api/oauth2/token', data, { headers }).then(fulfilled => {
-        console.log(fulfilled.data);
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(html);
+    const htmlPage = getHtmlPage(req.url.substring(1));
+    if (!htmlPage) {
+        res.write('U DONE FUCKED UP BOI');
         res.end();
-    }).catch(rejected => {
-        res.write('oops');
-        res.end();
-    });
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(htmlPage);
+    res.end();
 }).listen(8080);
+
+// http.createServer((req, res) => {
+//     const code = req.url.split('?code=')[1];
+//     console.log(req.url);
+//     console.log(code);
+//     const data = `client_id=&client_secret=&grant_type=authorization_code&redirect_uri=${encodeURIComponent('http://18.223.209.141:8080/')}&scope=${encodeURIComponent('identify gdm.join')}&code=${code}`;
+//     console.log(data);
+//     api.post('https://discordapp.com/api/oauth2/token', data, { headers }).then(fulfilled => {
+//         console.log(fulfilled.data);
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         res.write(getHtmlPage('registrationConfirmed'));
+//         res.end();
+//     }).catch(rejected => {
+//         res.write('oops');
+//         res.end();
+//     });
+// }).listen(8080);
 
 console.log('reddy');
