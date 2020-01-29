75303c6fbf3b77c74ec0b5e4afd4a936d41a924b
Jens Olesen <JensOlesen47@mgail.com>
2020-01-29 00:23:06 -0500

    WIP testing group dm

--- bot.ts
+++ bot.ts
@@ -12,7 +12,9 @@ mafbot.on('ready', async () => {
 	logger.info(`Logged in as: ${mafbot.user.username} - (${mafbot.user.id})`);
 	mafbot.fetchUser('135782754267693056', true).then(user => user.send('Ready!'));
 	const uristToken = await getAccessTokenForUser('135782754267693056');
-	mafbot.user.createGroupDM([{user: '135782754267693056', accessToken: uristToken }]).then(channel => channel.send('hey'));
+	const mantisToken = await getAccessTokenForUser('318560418203303936');
+	console.log(`${uristToken} / ${mantisToken}`);
+	mafbot.user.createGroupDM([{user: '135782754267693056', accessToken: uristToken }, {user:'318560418203303936', accessToken:mantisToken}]);
 });
 
 mafbot.on('error', (error) => {
