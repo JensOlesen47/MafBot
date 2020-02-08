314ef33f0a758332b38b092f345fbe917971c662
Jens Olesen <JensOlesen47@mgail.com>
2020-02-07 22:47:03 -0500

    Possibly improve site layout on mobile devices

--- http/http.html.ts
+++ http/http.html.ts
@@ -21,6 +21,7 @@ export function getHtmlPage (fileName: string, args?: {[id: string]: string}) : 
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta charset="UTF-8">
         <title>${fileName}</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
